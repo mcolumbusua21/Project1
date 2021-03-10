8acf42d2ff9b6a36812d11f8a75b967f5a977abd
Mark Columbus <mcolumbusua21@gmail.com>
2021-03-10 15:51:30 -0700

    navbar

---
+++ script.js
@@ -0,0 +1,4 @@
+var about = document.querySelector("#about")
+var recipes = document.querySelector("#recipes")
+var contact = document.querySelector("#contact")
+var issues = document.querySelector("#issues")
